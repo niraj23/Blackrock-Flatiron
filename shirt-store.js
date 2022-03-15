0ccd41dadf573323db1c307e8bf14794be8f4eff
Niraj <niraj2323@gmail.com>
2022-03-15 12:04:17 -0400

    finished with all test passing

--- shirt-store.js
+++ shirt-store.js
@@ -1,3 +1,29 @@
 export const cost = (shirts) => {
-  throw new Error("Please remove this statement and implement this function");
+  let discounted = {
+    5: 5 * 800 * 0.75,
+    4: 4 * 800 * 0.8,
+    3: 3 * 800 * 0.9,
+    2: 2 * 800 * 0.95,
+    1: 800,
+    0: 0,
+  };
+
+  let shirtsSet = [];
+
+  while (shirts.length > 0) {
+    shirtsSet.push(new Set(shirts).size);
+    new Set(shirts).forEach((elem) => {
+      shirts.splice(shirts.indexOf(elem), 1);
+    });
+  }
+
+  while (shirtsSet.includes(3) && shirtsSet.includes(5)) {
+    shirtsSet.splice(shirtsSet.indexOf(3), 1);
+    shirtsSet.splice(shirtsSet.indexOf(5), 1);
+    shirtsSet.push(4, 4);
+  }
+
+  let total = 0;
+  shirtsSet.forEach((elem) => (total += discounted[`${elem}`]));
+  return total;
 };
--- shirt-store.spec.js
+++ shirt-store.spec.js
@@ -7,82 +7,82 @@ describe("Shirt Store", () => {
       expect(cost(basket)).toEqual(800);
     });
 
-    xtest("two of the same shirt", () => {
+    test("two of the same shirt", () => {
       const basket = [2, 2];
       expect(cost(basket)).toEqual(1600);
     });
 
-    xtest("empty basket", () => {
+    test("empty basket", () => {
       const basket = [];
       expect(cost(basket)).toEqual(0);
     });
 
-    xtest("two different shirts", () => {
+    test("two different shirts", () => {
       const basket = [1, 2];
       expect(cost(basket)).toEqual(1520);
     });
 
-    xtest("three different shirts", () => {
+    test("three different shirts", () => {
       const basket = [1, 2, 3];
       expect(cost(basket)).toEqual(2160);
     });
 
-    xtest("four different shirts", () => {
+    test("four different shirts", () => {
       const basket = [1, 2, 3, 4];
       expect(cost(basket)).toEqual(2560);
     });
 
-    xtest("five different shirts", () => {
+    test("five different shirts", () => {
       const basket = [1, 2, 3, 4, 5];
       expect(cost(basket)).toEqual(3000);
     });
 
-    xtest("two groups of four is cheaper than group of five plus group of three", () => {
+    test("two groups of four is cheaper than group of five plus group of three", () => {
       const basket = [1, 1, 2, 2, 3, 3, 4, 5];
       expect(cost(basket)).toEqual(5120);
     });
 
-    xtest("two groups of four is cheaper than groups of five and three", () => {
+    test("two groups of four is cheaper than groups of five and three", () => {
       const basket = [1, 1, 2, 3, 4, 4, 5, 5];
       expect(cost(basket)).toEqual(5120);
     });
 
-    xtest("group of four plus group of two is cheaper than two groups of three", () => {
+    test("group of four plus group of two is cheaper than two groups of three", () => {
       const basket = [1, 1, 2, 2, 3, 4];
       expect(cost(basket)).toEqual(4080);
     });
 
-    xtest("two each of first 4 shirts and 1 copy each of rest", () => {
+    test("two each of first 4 shirts and 1 copy each of rest", () => {
       const basket = [1, 1, 2, 2, 3, 3, 4, 4, 5];
       expect(cost(basket)).toEqual(5560);
     });
 
-    xtest("two of each shirt", () => {
+    test("two of each shirt", () => {
       const basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
       expect(cost(basket)).toEqual(6000);
     });
 
-    xtest("three of first shirt and 2 each of remaining", () => {
+    test("three of first shirt and 2 each of remaining", () => {
       const basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1];
       expect(cost(basket)).toEqual(6800);
     });
 
-    xtest("three each of first 2 shirts and 2 each of remaining shirts", () => {
+    test("three each of first 2 shirts and 2 each of remaining shirts", () => {
       const basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2];
       expect(cost(basket)).toEqual(7520);
     });
 
-    xtest("four groups of four are cheaper than two groups each of five and three", () => {
+    test("four groups of four are cheaper than two groups each of five and three", () => {
       const basket = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5];
       expect(cost(basket)).toEqual(10240);
     });
 
-    xtest("two groups of four and a group of five", () => {
+    test("two groups of four and a group of five", () => {
       const basket = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5];
       expect(cost(basket)).toEqual(8120);
     });
 
-    xtest("shuffled shirt order", () => {
+    test("shuffled shirt order", () => {
       const basket = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3];
       expect(cost(basket)).toEqual(8120);
     });
